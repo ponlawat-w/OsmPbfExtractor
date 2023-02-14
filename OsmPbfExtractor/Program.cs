4554f4682473acd07e745c2b43fa47ffbdea4f48
ponlawat-w <ponlawat@whereismytransport.com>
2023-02-14 16:44:12 +0700

    Updated to net 6.0

--- OsmPbfExtractor/Program.cs
+++ OsmPbfExtractor/Program.cs
@@ -11,7 +11,7 @@ namespace OsmPbfExtractor
         static void Main(string[] args)
         {
             Console.Write("Source file: ");
-            string sourcePath = Console.ReadLine();
+            string sourcePath = Console.ReadLine().Trim();
             if (!File.Exists(sourcePath))
             {
                 Console.Error.WriteLine("File not found!");
@@ -19,19 +19,19 @@ namespace OsmPbfExtractor
             }
 
             Console.Write("Target file: ");
-            string targetPath = Console.ReadLine();
+            string targetPath = Console.ReadLine().Trim();
 
-            Console.Write("Northeast point [lat,lng]: ");
-            float[] northeast = Console.ReadLine().Split(',').Select(s => float.Parse(s)).ToArray();
-            if (northeast.Length < 2)
+            Console.Write("First corner point [lat,lng]: ");
+            float[] firstPoint = Console.ReadLine().Split(',').Select(s => float.Parse(s.Trim())).ToArray();
+            if (firstPoint.Length < 2)
             {
                 Console.Error.WriteLine("Invalid input!");
                 return;
             }
 
-            Console.Write("Southwest point [lat,lng]: ");
-            float[] southwest = Console.ReadLine().Split(',').Select(s => float.Parse(s)).ToArray();
-            if (southwest.Length < 2)
+            Console.Write("Second corner point [lat,lng]: ");
+            float[] secondPoint = Console.ReadLine().Split(',').Select(s => float.Parse(s.Trim())).ToArray();
+            if (secondPoint.Length < 2)
             {
                 Console.Error.WriteLine("Invalid input!");
                 return;
@@ -47,8 +47,11 @@ namespace OsmPbfExtractor
             Console.Write("Openning source...");
             using (FileStream sourceStream = new FileInfo(sourcePath).OpenRead())
             {
-                OsmStreamSource osmSource = new PBFOsmStreamSource(sourceStream).FilterBox(
-                    northeast[1], northeast[0], southwest[1], southwest[0]);
+                float left = Math.Min(firstPoint[1], secondPoint[1]);
+                float top = Math.Max(firstPoint[0], secondPoint[0]);
+                float right = Math.Max(firstPoint[1], secondPoint[1]);
+                float bottom = Math.Min(firstPoint[0], secondPoint[0]);
+                OsmStreamSource osmSource = new PBFOsmStreamSource(sourceStream).FilterBox(left, top, right, bottom, true);
                 Console.WriteLine("OK");
 
                 long i = 0;
